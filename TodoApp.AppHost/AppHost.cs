8846e4127f591ca9bdbe5a8889a5ea7a86349036
Daniel <3842788+dscpinheiro@users.noreply.github.com>
2025-08-31 11:40:53 -0400

    Add scaffold for front-end project

--- TodoApp.AppHost/AppHost.cs
+++ TodoApp.AppHost/AppHost.cs
@@ -1,8 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSqlite("db").WithSqliteWeb();
+
 var backend = builder.AddProject<Projects.TodoApp_Backend>("backend")
     .WithReference(db)
     .WaitFor(db);
+
+builder.AddViteApp(name: "frontend", workingDirectory: "../TodoApp.Frontend")
+    .WithReference(backend)
+    .WaitFor(backend)
+    .WithNpmPackageInstallation();
 
 builder.Build().Run();
