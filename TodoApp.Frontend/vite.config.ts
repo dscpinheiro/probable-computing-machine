ddb023379b2ed0c3bbb0e4b4889785b3bceed510
Daniel <3842788+dscpinheiro@users.noreply.github.com>
2025-08-31 12:05:21 -0400

    Update frontend to get backend URL

--- TodoApp.Frontend/vite.config.ts
+++ TodoApp.Frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Environment variable set by Aspire.
+  // In a PROD environment, this would also be HTTPS and configured by whatever deployment mechanism used.
+  const backendUrl = env.services__backend__http__0;
+  if (!backendUrl) {
+    throw new Error('Backend for the TODOs API is not defined');
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/todos': {
+          target: backendUrl,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  }
+});
